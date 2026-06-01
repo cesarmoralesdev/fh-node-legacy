22cc9b7cdf8f8771a569b393f9060e4f15087d1b
Cesar Morales Dev <cesar.morales.dev@gmail.com>
2026-05-31 23:33:03 -0500

    127.- Modelo de Usuario

---
+++ 08-alcances-rest-server-mantenimiento-usuario/models/usuario.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+
+const UsuarioSchema = Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    correo: {
+        type: String,
+        required: [true, 'El correo es obligatorio'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es obligatoria'],
+        unique: true
+    },
+    img: {
+        type: String,
+    },
+    role: {
+        type: String,
+        required: true,
+        emun: ['ADMIN_ROLE', 'USER_ROLE']
+    },
+    status: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    },
+});
+
+module.exports = model('Usuario', UsuarioSchema);
