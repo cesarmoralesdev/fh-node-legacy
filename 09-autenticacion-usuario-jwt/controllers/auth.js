dfa7c8ee6db080eae913efdaa8dedb0df95beef7
Cesar Morales Dev <cesar.morales.dev@gmail.com>
2026-06-07 21:37:13 -0500

    150.- Login de usuario

--- 09-autenticacion-usuario-jwt/controllers/auth.js
+++ 09-autenticacion-usuario-jwt/controllers/auth.js
@@ -1,11 +1,45 @@
 const { request, response } = require("express");
+const bcryptjs = require('bcryptjs');
+const Usuarion = require('../models/usuario');
 
-const login = (req = request, res = response) => {
+const login = async (req = request, res = response) => {
+    const { correo, password } = req.body;
+    try {
+        // Verificar si el email existe
+        const usuario = await Usuarion.findOne({ correo });
+        if (!usuario) {
+            return res.status(400).json({
+                msg: 'Usuario / password no son correctos - correo'
+            })
+        }
+        // Si el usuario estaa activo
+        if (!usuario.status) {
+            return res.status(400).json({
+                msg: 'Usuario / password no son correctos - status = false'
+            })
+        }
+        // Verificar la contraseña
+        const validPassword = bcryptjs.compareSync(password, usuario.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                msg: 'Usuario / password no son correctos - password'
+            })
+        }
 
 
-    res.json({
-        msg: 'Login OK'
-    });
+        // Generar el JWT
+
+
+
+        res.json({
+            msg: 'Login OK'
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Hable con el administrador'
+        });
+    }
 }
 
 module.exports = {
